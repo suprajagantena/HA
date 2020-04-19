1b37e5bd349773c84d9ad2146ee2e968e7ec7633
suprajagantena <suprajagantena@gmail.com>
2020-04-19 19:45:46 -0400

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "pub" {
     vpc_id = "aws_vpc.dev.id"
     cidr_block = "10.192.1.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "us-east-1a"
+    #availability_zone = "us-east-1a"
     tags = {
     Name = "pubsubnet"
    }
