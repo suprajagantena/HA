8bc890cb1592628fd1fdf166a0d338a89b70e0d8
suprajagantena <suprajagantena@gmail.com>
2020-04-19 19:22:49 -0400

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -3,7 +3,7 @@ resource "aws_subnet" "pub" {
     cidr_block = "10.192.1.0/24"
     map_public_ip_on_launch = "true"
     availability_zone = "eu-east-1a"
-    tags {
-        Name = "dev-subnet-public-1"
-    }
+   # tags {
+   #    Name = "dev-subnet-public-1"
+   # }
 }
