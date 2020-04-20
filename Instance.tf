4d4a945ccff68cc744b72ccb8793dc2d5b6a90db
suprajagantena <suprajagantena@gmail.com>
2020-04-20 13:39:44 -0400

    Update Instance.tf

--- Instance.tf
+++ Instance.tf
@@ -3,9 +3,5 @@ resource "aws_instance" "web1" {
     instance_type = "t2.micro"
     subnet_id = "aws_subnet.pri.id"
     vpc_security_group_ids = "aws_security_group.ssh.id"
-    key_name = "${aws_key_pair.london-region-key-pair.id}"
-    connection {
-        user = "${var.EC2_USER}"
-        private_key = "${file("${var.PRIVATE_KEY_PATH}")}"
-    }
+    
 }
