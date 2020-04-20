ea5bb986495f7547a36acadda1f23a89bd2b46bb
suprajagantena <suprajagantena@gmail.com>
2020-04-19 23:11:30 -0400

    Update Routetable.tf

--- Routetable.tf
+++ Routetable.tf
@@ -2,7 +2,7 @@ resource "aws_route_table" "dev-routetable" {
     vpc_id = aws_vpc.dev.id
     
     route {
-        cidr_block = "10.192.0.0/16"
+        cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.dev-igw.id
     }
     
