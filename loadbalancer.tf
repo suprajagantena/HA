99314a1f46eb97772517465d2afb4f0adeb3e9c2
suprajagantena <suprajagantena@gmail.com>
2020-04-20 14:45:00 -0400

    Update loadbalancer.tf

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -1,9 +1,9 @@
-resource "aws_lb" "test" {
+resource "aws_lb" "lb" {
   name               = "test-lb-tf"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = ["${aws_security_group.lb_sg.id}"]
-  subnets            = ["${aws_subnet.public.*.id}"]
+  security_groups    = "aws_security_group.lb_sg.id"
+  subnets            = "aws_subnet.public.*.id"
 
   enable_deletion_protection = true
 
