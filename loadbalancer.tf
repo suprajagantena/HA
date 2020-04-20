a8a9df5ecf6da1fee89d068949db369cd5c8ff46
suprajagantena <suprajagantena@gmail.com>
2020-04-20 15:07:12 -0400

    Update loadbalancer.tf

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -2,18 +2,18 @@ resource "aws_lb" "lb" {
   name               = "test-lb-tf"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = "aws_security_group.lb_sg.id"
+  security_groups    = "aws_security_group.ssh.id"
   subnets            = "aws_subnet.pub.id"
 
   enable_deletion_protection = true
 
   access_logs {
-    bucket  = "${aws_s3_bucket.lb_logs.bucket}"
+    bucket  = "aws_s3_bucket.lb_logs.bucket"
     prefix  = "test-lb"
     enabled = true
   }
 
   tags = {
-    Environment = "production"
+    Environment = "dev"
   }
 }
