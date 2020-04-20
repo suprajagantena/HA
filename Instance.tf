c25f9f5a15ded9bd62a5f64224b46b5acaba5a74
suprajagantena <suprajagantena@gmail.com>
2020-04-20 13:48:03 -0400

    Update Instance.tf

--- Instance.tf
+++ Instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "dev" {
-    ami = "${lookup(var.AMI, var.AWS_REGION)}"
+    ami = "lookup(var.AMI, var.AWS_REGION)"
     instance_type = "t2.micro"
     subnet_id = "aws_subnet.pri.id"
     vpc_security_group_ids = "aws_security_group.ssh.id"
