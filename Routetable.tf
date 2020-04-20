bd6820eba16203f635561698fac6f7cf8cc22ed0
suprajagantena <suprajagantena@gmail.com>
2020-04-20 15:22:42 -0400

    Update Routetable.tf

--- Routetable.tf
+++ Routetable.tf
@@ -11,7 +11,9 @@ resource "aws_route_table" "dev-routetable" {
     }
 }
 resource "aws_route_table_association" "dev-public-subnet"{
-    subnet_id = "aws_subnet.pub.id"
-    route_table_id = "aws_route_table.dev-routetable.id"
+    subnet_id = aws_subnet.pub.id
+    route_table_id = aws_route_table.dev-routetable.id
 }
+
+
 
