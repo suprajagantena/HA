70e998ae43c7506d39af0ead926609e583166b1b
suprajagantena <suprajagantena@gmail.com>
2020-04-20 12:34:45 -0400

    Update Instance.tf

--- Instance.tf
+++ Instance.tf
@@ -1,8 +1,7 @@
 resource "aws_instance" "web1" {
     ami = "${lookup(var.AMI, var.AWS_REGION)}"
     instance_type = "t2.micro"
-    # VPC
-    subnet_id = "${aws_subnet.prod-subnet-public-1.id}"
+    subnet_id = "aws_subnet.dev-subnet-public-1.id}"
     # Security Group
     vpc_security_group_ids = ["${aws_security_group.ssh-allowed.id}"]
     # the Public SSH key
