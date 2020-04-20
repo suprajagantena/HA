21bd50ea10cc4aac6da4cabf54db554213edafa9
suprajagantena <suprajagantena@gmail.com>
2020-04-20 12:41:40 -0400

    Update Instance.tf

--- Instance.tf
+++ Instance.tf
@@ -2,26 +2,10 @@ resource "aws_instance" "web1" {
     ami = "${lookup(var.AMI, var.AWS_REGION)}"
     instance_type = "t2.micro"
     subnet_id = "aws_subnet.pri.id"
-    vpc_security_group_ids = ["${aws_security_group.ssh-allowed.id}"]
-    # the Public SSH key
+    vpc_security_group_ids = "aws_security_group.ssh.id"
     key_name = "${aws_key_pair.london-region-key-pair.id}"
-    # nginx installation
-    provisioner "file" {
-        source = "nginx.sh"
-        destination = "/tmp/nginx.sh"
-    }
-    provisioner "remote-exec" {
-        inline = [
-             "chmod +x /tmp/nginx.sh",
-             "sudo /tmp/nginx.sh"
-        ]
-    }
     connection {
         user = "${var.EC2_USER}"
         private_key = "${file("${var.PRIVATE_KEY_PATH}")}"
     }
 }
-// Sends your public key to the instance
-resource "aws_key_pair" "london-region-key-pair" {
-    key_name = "london-region-key-pair"
-    public_key = "${file(var.PUBLIC_KEY_PATH)}"
