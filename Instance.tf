27fd8c0f7f9cabf6a0f6ba2f177a12808e7adb5e
suprajagantena <suprajagantena@gmail.com>
2020-04-20 13:46:12 -0400

    Update Instance.tf

--- Instance.tf
+++ Instance.tf
@@ -1,7 +1,12 @@
-resource "aws_instance" "web1" {
+resource "aws_instance" "dev" {
     ami = "${lookup(var.AMI, var.AWS_REGION)}"
     instance_type = "t2.micro"
     subnet_id = "aws_subnet.pri.id"
     vpc_security_group_ids = "aws_security_group.ssh.id"
-    
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
+
+
