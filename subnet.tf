b05018fffaecc2553e21af43593a157b8a1567ad
suprajagantena <suprajagantena@gmail.com>
2020-04-19 21:12:58 -0400

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -9,7 +9,7 @@ resource "aws_subnet" "pub" {
    }
 resource "aws_subnet" "pri" {
     vpc_id = aws_vpc.dev.id
-    cidr_block = "10.192.2.0/32"
+    cidr_block = "10.192.2.0/24"
     map_public_ip_on_launch = "false"
     #availability_zone = "us-east-1b"
     tags = {
