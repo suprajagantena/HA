9310b967132b0cab5c54cc2ef63b9ba3c57de1b7
suprajagantena <suprajagantena@gmail.com>
2020-04-21 15:39:04 -0400

    Update Instance.tf

--- Instance.tf
+++ Instance.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "dev" {
     ami = "data.aws_ami.Linux.id"
     instance_type = "t2.micro"
-    subnet_id = "aws_subnet.pri.id"
-    vpc_security_group_ids = "aws_security_group.ssh.id"
+    subnet_id = ["aws_subnet.pri.id"]
+    vpc_security_group_ids = ["aws_security_group.ssh.id"]
 
   tags = {
     Name = "HelloWorld"
