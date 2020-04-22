1d9648ae1723d8ddfb57e3e67a51b183b937d2c9
suprajagantena <suprajagantena@gmail.com>
2020-04-21 23:08:48 -0400

    Update launchconfig.tf

--- launchconfig.tf
+++ launchconfig.tf
@@ -1,5 +1,5 @@
 resource "aws_launch_configuration" "as_conf" {
 name          = "web_config"
-  image_id      = "data.aws_ami.Linux.id"
+  image_id      = "ami-0323c3dd2da7fb37d"
   instance_type = "t2.micro"
 }
