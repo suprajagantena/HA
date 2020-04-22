257bdef125c80ff6bff2de3530cd92d236724c53
suprajagantena <suprajagantena@gmail.com>
2020-04-21 23:11:01 -0400

    Update and rename Instance to Instance.tf

--- Instance.tf
+++ Instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "dev" {
-    ami = "data.aws_ami.Linux.id"
+    ami = "ami-0323c3dd2da7fb37d"
     instance_type = "t2.micro"
     subnet_id = ["aws_subnet.pri.id"]
     vpc_security_group_ids = ["aws_security_group.ssh.id"]
