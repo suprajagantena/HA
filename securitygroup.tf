f08db0193940e95aa73d5a1c95a9eeb4da71480a
suprajagantena <suprajagantena@gmail.com>
2020-04-27 13:41:07 -0400

    Update securitygroup.tf

--- securitygroup.tf
+++ securitygroup.tf
@@ -19,6 +19,11 @@ resource "aws_security_group" "new" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
     tags = {
         Name = "new"
     }
