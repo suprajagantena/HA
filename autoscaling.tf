c76c089324693efe3062a795bcb2f41186ebf34a
suprajagantena <suprajagantena@gmail.com>
2020-04-30 17:15:48 -0400

    Update autoscaling.tf

--- autoscaling.tf
+++ autoscaling.tf
@@ -10,7 +10,7 @@ resource "aws_launch_configuration" "mylc" {
 
 resource "aws_autoscaling_group" "myasg" {
   name                 = "terraform-myasg"
-  launch_configuration = "aws_launch_configuration.as_conf.name"
+  launch_configuration = "aws_launch_configuration.mylc"
   min_size             = 2
   max_size             = 2
 
