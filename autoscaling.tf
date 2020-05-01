7bb2e80494009c8ff508c527d2a77fa957e24fea
suprajagantena <suprajagantena@gmail.com>
2020-05-01 13:38:00 -0400

    Update autoscaling.tf

--- autoscaling.tf
+++ autoscaling.tf
@@ -11,10 +11,11 @@ resource "aws_launch_configuration" "mylc" {
 resource "aws_autoscaling_group" "myasg" {
   name                 = "terraform-myasg"
   launch_configuration = "${aws_launch_configuration.mylc.name}"
+  wait_for_capacity_timeout = "8m"
   vpc_zone_identifier  = ["${aws_subnet.pri.id}","${aws_subnet.pri.id}"]
   min_size             = 2
   max_size             = 2
-
+  
   lifecycle {
     create_before_destroy = true
   }
