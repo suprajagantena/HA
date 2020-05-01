650f4626751f1ade490a166b2122e421c352f684
suprajagantena <suprajagantena@gmail.com>
2020-05-01 11:40:13 -0400

    Update autoscaling.tf

--- autoscaling.tf
+++ autoscaling.tf
@@ -3,8 +3,7 @@ resource "aws_launch_configuration" "mylc" {
   image_id      =  "ami-0323c3dd2da7fb37d"  
   instance_type = "t2.micro"
   
-
-  lifecycle {
+    lifecycle {
     create_before_destroy = true
   }
 }
@@ -12,7 +11,7 @@ resource "aws_launch_configuration" "mylc" {
 resource "aws_autoscaling_group" "myasg" {
   name                 = "terraform-myasg"
   launch_configuration = "${aws_launch_configuration.mylc.name}"
-  vpc_zone_identifier  = ["${aws_subnet.pri.id}"]
+  vpc_zone_identifier  = ["aws_subnet.pri.id"]
   min_size             = 2
   max_size             = 2
 
